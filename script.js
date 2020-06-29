77c10d6de8c0a8a761389d6b18a0b4830234c8f4
anmarkosyan <anush.markos@gmail.com>
2020-06-29 12:06:54 +0400

    changed some detalis

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 //alert('hi there')
 //using JQuery
-$(document).ready(function () {
-    $('.header').height($(window).height());
-})
-$(".navbar a").click(function(){
-    $("body,html").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-
-})
